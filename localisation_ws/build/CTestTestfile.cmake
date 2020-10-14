0cc9fbe8663bb069e8682d157d6fcdc8e7b9ded9
James P M <jamespmnayak@gmail.com>
2020-10-14 23:50:50 +0530

    initial commit

---
+++ localisation_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/james/localisation_ws/src
+# Build directory: /home/james/localisation_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("where_am_i")
---
+++ localisation_ws/build/where_am_i/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/james/localisation_ws/src/where_am_i
+# Build directory: /home/james/localisation_ws/build/where_am_i
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
